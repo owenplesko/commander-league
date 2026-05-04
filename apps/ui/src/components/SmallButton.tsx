5d5ec22523da0524343be6351d331aa9af6973a1
owen <owenplesko@gmail.com>
2026-05-04 12:32:21 -0400

    unused components cleanup

--- apps/ui/src/components/SmallButton.tsx
+++
@@ -1,9 +0,0 @@
-import classes from "./SmallButton.module.css";
-
-export function SmallButton() {
-  return (
-    <button className={classes["small-button"]}>
-      <i className="pi pi-times" />
-    </button>
-  );
-}
