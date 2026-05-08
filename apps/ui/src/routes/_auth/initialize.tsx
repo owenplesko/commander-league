04ef5079102fe6414e8371a3814da9daef43fc15
owen <owenplesko@gmail.com>
2026-05-08 14:51:43 -0400

    init form

--- apps/ui/src/routes/_auth/initialize.tsx
+++ apps/ui/src/routes/_auth/initialize.tsx
@@ -1,9 +1,50 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { FormInputText } from "@/features/forms/FormInputText";
+import { orpc } from "@/lib/client";
+import { useMutation } from "@tanstack/react-query";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { Button } from "primereact/button";
+import { Card } from "primereact/card";
+import { useForm, type SubmitHandler } from "react-hook-form";
 
-export const Route = createFileRoute('/_auth/initialize')({
+export const Route = createFileRoute("/_auth/initialize")({
   component: RouteComponent,
-})
+});
+
+type FormData = {
+  name: string;
+};
 
 function RouteComponent() {
-  return <div>Hello "/_auth/initialize"!</div>
+  const router = useRouter();
+  const { control, handleSubmit } = useForm<FormData>();
+
+  const mutation = useMutation(orpc.league.initialize.mutationOptions());
+
+  const onSubmit: SubmitHandler<FormData> = async ({ name }) => {
+    await mutation.mutateAsync({ name });
+    router.navigate({ to: "/" });
+  };
+
+  return (
+    <div style={{ display: "grid", placeItems: "center" }}>
+      <Card
+        title="League Initialization"
+        footer={<Button form="league-init" label="Initialize" type="submit" />}
+        style={{ width: "30rem", margin: "4rem" }}
+      >
+        <form
+          id="league-init"
+          className="modalForm"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <FormInputText
+            label="Name"
+            name="name"
+            placeholder="league name..."
+            control={control}
+          />
+        </form>
+      </Card>
+    </div>
+  );
 }
